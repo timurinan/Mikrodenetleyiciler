fbdde207c655c210608eb796a3ce411991d42fa7
timurinan <91524596+timurinan@users.noreply.github.com>
2024-11-13 14:34:59 +0300

    Update Uygulama11.c

--- Ders Ornekleri/Uygulama11.c
+++ Ders Ornekleri/Uygulama11.c
@@ -7,8 +7,8 @@
 void main() {
      TRISB=0;
      PORTB=0;
-     TMR0=216;//250 KERE SAYACAK
-     OPTION_REG=0; //0X01 YA DA 1
+     TMR0=216;//216 KERE SAYACAK
+     OPTION_REG=0; //Prescale 2
      INTCON=0XA0;    //0XA0 YA DA 64
      
      while(1){
